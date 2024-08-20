3841293c027158a6d43b1db42631197ba071107d
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-08-20 16:22:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# HeadlessMc-Container2WASM
+
+This page takes the [container2wasm](https://github.com/ktock/container2wasm) demo
+from [here](https://github.com/ktock/container2wasm-demo) and uses it to run a
+[HeadlessMc](https://github.com/3arthqu4ke/headlessmc) Docker image inside the browser.
+This is just for fun and runs about as well as you would
+expect from three VMs stacked on top of each other.
+
+Page: https://3arthqu4ke.github.io/hmc-container2wasm/
+
+## Building images
+
+Run `./create-images.sh`
+
+container2wasm needs to be available on the host.
+
+## License
+
+Apache License Version 2.0.
+
+This repository contains code copied from https://github.com/ktock/container2wasm-demo.
+Changes: The debian, python and vim images have been removed for a HeadlessMc image.
+
+Additionally, this repository relies on third-pirty softwares:
+
+- Bootstrap ([MIT License](https://github.com/twbs/bootstrap/blob/main/LICENSE)): https://github.com/twbs/bootstrap
+- xterm-pty ([MIT License](https://github.com/mame/xterm-pty/blob/main/LICENSE.txt)): https://github.com/mame/xterm-pty
+- xterm.js ([MIT License](https://github.com/xtermjs/xterm.js/blob/master/LICENSE)): https://github.com/xtermjs/xterm.js
+- coi-serviceworker.js([MIT License](https://github.com/gzuidhof/coi-serviceworker/blob/master/LICENSE)): https://github.com/gzuidhof/coi-serviceworker
+- `browser_wasi_shim` (either of [MIT License](https://github.com/bjorn3/browser_wasi_shim/blob/main/LICENSE-MIT) and [Apache License 2.0](https://github.com/bjorn3/browser_wasi_shim/blob/main/LICENSE-APACHE)): https://github.com/bjorn3/browser_wasi_shim
+- container2wasm-genearted images:
+  - Containers
+    - alpine-based containers: https://pkgs.alpinelinux.org/packages
+    - debian-based containers: see `/usr/share/doc/*/copyright`
+  - Other dependencies(emulator, etc): https://github.com/ktock/container2wasm#acknowledgement
---
+++ create-images.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SOURCE=./src/
+DEST=./out/
+WASI_MAX_CHUNK=50MB
+C2W=c2w
+C2W_EXTRA_FLAGS_V=${C2W_EXTRA_FLAGS:-}
+
+# /image : image name
+# /Dockerfile : dockerfile to use
+# /target : target compiler (wasi, emscripten, default: wasi)
+# /arch : image architecture (default: amd64)
+
+for I in $(ls -1 ${SOURCE}) ;
+do
+    OUTPUT_NAME="${I}-container"
+    if [ $(cat "${SOURCE}/${I}/target" || true) == "emscripten" ] ; then
+        TARGETARCH=$(cat "${SOURCE}/${I}/arch" || true)
+        if [ "${TARGETARCH}" == "" ] ; then
+            TARGETARCH="amd64"
+        fi
+        if [ -f "${SOURCE}/${I}/image" ]; then
+            ${C2W} --to-js --target-arch="${TARGETARCH}" ${C2W_EXTRA_FLAGS_V} --build-arg JS_OUTPUT_NAME=${OUTPUT_NAME} "$(cat ${SOURCE}/${I}/image)" "${DEST}"
+        elif [ -f "${SOURCE}/${I}/Dockerfile" ]; then
+            cat ${SOURCE}/${I}/Dockerfile | docker buildx build --progress=plain -t ${I} --platform="linux/${TARGETARCH}" --load -
+            ${C2W} --to-js --target-arch="${TARGETARCH}" ${C2W_EXTRA_FLAGS_V} --build-arg JS_OUTPUT_NAME=${OUTPUT_NAME} "${I}" "${DEST}"
+        else
+            echo "no image source found for ${I}"
+            exit 1
+        fi
+    else
+        TARGETARCH=$(cat "${SOURCE}/${I}/arch" || true)
+        if [ "${TARGETARCH}" == "" ] ; then
+            TARGETARCH="amd64"
+        fi
+        if [ -f "${SOURCE}/${I}/image" ]; then
+            ${C2W} --target-arch="${TARGETARCH}" ${C2W_EXTRA_FLAGS_V} "$(cat ${SOURCE}/${I}/image)" "${DEST}/${OUTPUT_NAME}.wasm"
+        elif [ -f "${SOURCE}/${I}/Dockerfile" ]; then
+            cat ${SOURCE}/${I}/Dockerfile | docker buildx build --progress=plain -t ${I} --platform="linux/${TARGETARCH}" --load -
+            ${C2W} --target-arch="${TARGETARCH}" ${C2W_EXTRA_FLAGS_V} "${I}" "${DEST}/${OUTPUT_NAME}.wasm"
+        else
+            echo "no image source found for ${I}"
+            exit 1
+        fi
+        split -d -b "${WASI_MAX_CHUNK}" --additional-suffix=.wasm "${DEST}/${OUTPUT_NAME}.wasm" "${DEST}/${OUTPUT_NAME}"
+        rm "${DEST}/${OUTPUT_NAME}.wasm"
+    fi
+done
